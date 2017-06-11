5531437857b8750450970025624b34b4e988c2cd
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-06-11 01:01:09 -0400

    change structure

--- html-scraper/index.js
+++ html-scraper/index.js
@@ -2,35 +2,18 @@
 
 import request from 'request';
 
-import { HEADERS, BASE_URL_ITEM, URL_ITEM_SUMMARY} from './constants';
-import { getSessionCookie } from './utils';
+import utils from './utils';
 import htmlParser from './html-parser';
 
-let myJar = request.jar();
-
 function requestData(itemId, sessionId, cb) {
-  let cookie = request.cookie( getSessionCookie(sessionId) );
-  let urlItemView = BASE_URL_ITEM + itemId;
-
-  myJar.setCookie(cookie, urlItemView);
-
-  let options = {
-    url: urlItemView,
-    jar: myJar,
-    headers: HEADERS
-  };
+  let optionsMainPage = utils.getOptionsMainPage(itemId, request, sessionId);
+  let optionsDetailsPage = utils.getOptionsDetailsPage(request, sessionId);
 
   // First request to setup the next request. We don't care about the data.
   // The server setup the id for the next loading page.
-  request(options, function (error, response, body) {
-    let options = {
-      url: URL_ITEM_SUMMARY,
-      jar: myJar,
-      headers: HEADERS
-    };
-
+  request(optionsMainPage, function (error, response, body) {
     // Second request to get the real data
-    request(options, function (error, response, body) {
+    request(optionsDetailsPage, function (error, response, body) {
       console.log('htmlParser.getParsedData(body)', htmlParser.getParsedData(body))
       cb( htmlParser.getParsedData(body) )
     });
--- html-scraper/utils.js
+++ html-scraper/utils.js
@@ -1,10 +1,37 @@
 'use strict';
 
-function getSessionCookie(sessionId) {
+import { HEADERS, BASE_URL_ITEM, URL_ITEM_SUMMARY} from './constants';
+
+function _getSessionCookie(sessionId) {
   // example : 'JSESSIONID=0000B1nOCCiobYRnmVj_Ui8VAxE:19tl92die'
   return 'JSESSIONID=' + sessionId;
 }
 
+function getOptionsMainPage(itemId, request, sessionId) {
+  let urlItemView = BASE_URL_ITEM + itemId;
+  let cookie = request.cookie( _getSessionCookie(sessionId) );
+  let myJar = request.jar();
+  myJar.setCookie(cookie, urlItemView);
+  return {
+    url: urlItemView,
+    jar: myJar,
+    headers: HEADERS
+  }
+}
+
+function getOptionsDetailsPage(request, sessionId) {
+  let cookie = request.cookie( _getSessionCookie(sessionId) );
+  let myJar = request.jar();
+  myJar.setCookie(cookie, URL_ITEM_SUMMARY);
+  return {
+    url: URL_ITEM_SUMMARY,
+    jar: myJar,
+    headers: HEADERS
+  };
+}
+
+
 module.exports = {
-  getSessionCookie
+  getOptionsMainPage,
+  getOptionsDetailsPage
 }
