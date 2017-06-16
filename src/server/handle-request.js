078b205839feffaf012e7525ef11804c3ed8a453
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-06-15 22:12:27 -0400

    add server dir

---
+++ src/server/handle-request.js
@@ -0,0 +1,60 @@
+'use strict';
+
+import htmlScraper from '../html-scraper';
+import utils from './utils';
+import mock from './mock.json';
+import unitPriceConverter from '../unit-price-converter';
+
+const IS_MOCK = true;
+
+function _emitFromHmlScraper(id, sessionId, socket) {
+  let date = utils.formatDate(new Date());
+  htmlScraper.requestData(id, sessionId, function cb(parsedData) {
+    let allData = Object.assign(parsedData, {
+      id: id,
+      date: date
+    });
+    socket.emit('testerEvent', allData);
+  }); // end requestData()
+}
+
+function _emitFromMock(i, socket) {
+  let date = utils.formatDate(new Date());
+  let parsedData = mock[i];
+  let { id, price, packetFormat } = parsedData;
+
+  let { unitPriceFormated, standardUnit } =
+      unitPriceConverter.getStandardPriceFormat(packetFormat, price);
+
+  let allData = Object.assign(parsedData, {
+    id: id,
+    unitPriceFormated: unitPriceFormated,
+    standardUnit: standardUnit,
+    date: date
+  });
+  socket.emit('testerEvent', allData);
+}
+
+function emitToClient(dataClient, socket, isMock) {
+  let { sessionId, ids } = dataClient;
+
+  if (!(ids && ids.length > 0)) {
+    return null;
+  }
+
+  for (let i = 0; i < ids.length; i++) {
+    setTimeout(function() {
+      if (isMock) {
+        _emitFromMock(i, socket);
+      } else {
+        let id = ids[i];
+        _emitFromHmlScraper(id, sessionId, socket);
+      }
+    }, i * 1000); // end setTimeout()
+  } // end for()
+
+}
+
+module.exports = {
+  emitToClient
+}
---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import path from 'path';
+
+
+import handleRequest from './handle-request';
+
+const IS_MOCK = true;
+
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+  console.log('A user connected');
+
+  socket.on('clientEvent', function(jsonData) {
+    let dataClient = JSON.parse(jsonData);
+    handleRequest.emitToClient(dataClient, socket, IS_MOCK);
+  }); // end clientEvent()
+
+  socket.on('disconnect', function() {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
