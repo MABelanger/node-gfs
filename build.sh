0a13fdb4b8d11badea7e23dd54d0fc51c6d4c82a
Michel-Alexandre <michel.alexandre.belanger@gmail.com>
2017-06-19 21:27:21 -0400

    build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+npm i &&
+git submodule init &&
+git submodule update &&
+cd unit-price-hunter-client &&
+npm i &&
+npm run build &&
+cp -a dist/ ../src/server/ &&
+cd ../ &&
+npm run server
